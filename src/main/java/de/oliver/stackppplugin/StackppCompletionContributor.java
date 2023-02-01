5feb2cff4b892ef8022c78c88b7de6cb97acaa30
Oliver <79666085+OliverSchlueter@users.noreply.github.com>
2023-02-01 09:56:06 +0100

    Add more auto completions

--- src/main/java/de/oliver/stackppplugin/StackppCompletionContributor.java
+++ src/main/java/de/oliver/stackppplugin/StackppCompletionContributor.java
@@ -14,39 +14,39 @@ public class StackppCompletionContributor extends CompletionContributor {
                 new CompletionProvider<>() {
                     @Override
                     protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet resultSet) {
-                            resultSet.addElement(LookupElementBuilder.create("move").withTailText(" <val> <reg>"));
-                            resultSet.addElement(LookupElementBuilder.create("push"));
-                            resultSet.addElement(LookupElementBuilder.create("pop"));
-                            resultSet.addElement(LookupElementBuilder.create("add"));
-                            resultSet.addElement(LookupElementBuilder.create("subtract"));
-                            resultSet.addElement(LookupElementBuilder.create("multiply"));
-                            resultSet.addElement(LookupElementBuilder.create("divide"));
-                            resultSet.addElement(LookupElementBuilder.create("modulo"));
-                            resultSet.addElement(LookupElementBuilder.create("lshift"));
-                            resultSet.addElement(LookupElementBuilder.create("rshift"));
-                            resultSet.addElement(LookupElementBuilder.create("and"));
-                            resultSet.addElement(LookupElementBuilder.create("or"));
-                            resultSet.addElement(LookupElementBuilder.create("xor"));
-                            resultSet.addElement(LookupElementBuilder.create("not"));
-                            resultSet.addElement(LookupElementBuilder.create("print"));
-                            resultSet.addElement(LookupElementBuilder.create("aprint"));
+                            resultSet.addElement(LookupElementBuilder.create("move").withTailText(" <val> <reg>").withTypeText("moves a value into a register"));
+                            resultSet.addElement(LookupElementBuilder.create("push").withTailText(" <val>").withTypeText("pushes a value onto the stack"));
+                            resultSet.addElement(LookupElementBuilder.create("pop").withTailText(" <reg>").withTypeText("pops a value from the stack into a register"));
+                            resultSet.addElement(LookupElementBuilder.create("add").withTailText(" <reg> <val>").withTypeText("adds the value to the register"));
+                            resultSet.addElement(LookupElementBuilder.create("subtract").withTailText(" <reg> <val>").withTypeText("subtracts the value from the register"));
+                            resultSet.addElement(LookupElementBuilder.create("multiply").withTailText(" <reg> <val>").withTypeText("multiples the value with the register"));
+                            resultSet.addElement(LookupElementBuilder.create("divide").withTailText(" <reg> <val>").withTypeText("divides the value with the register"));
+                            resultSet.addElement(LookupElementBuilder.create("modulo").withTailText(" <reg> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("lshift").withTailText(" <reg> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("rshift").withTailText(" <reg> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("and").withTailText(" <reg> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("or").withTailText(" <reg> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("xor").withTailText(" <reg> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("not").withTailText(" <reg>"));
+                            resultSet.addElement(LookupElementBuilder.create("print").withTailText(" <reg>"));
+                            resultSet.addElement(LookupElementBuilder.create("aprint").withTailText(" <val>"));
                             resultSet.addElement(LookupElementBuilder.create("prints"));
-                            resultSet.addElement(LookupElementBuilder.create("if"));
+                            resultSet.addElement(LookupElementBuilder.create("if").withTailText(" <val> <operator> <val>"));
                             resultSet.addElement(LookupElementBuilder.create("else"));
-                            resultSet.addElement(LookupElementBuilder.create("while"));
-                            resultSet.addElement(LookupElementBuilder.create("function"));
-                            resultSet.addElement(LookupElementBuilder.create("call"));
+                            resultSet.addElement(LookupElementBuilder.create("while").withTailText(" <val> <operator> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("function").withTailText(" <name>"));
+                            resultSet.addElement(LookupElementBuilder.create("call").withTailText(" <function name>"));
                             resultSet.addElement(LookupElementBuilder.create("continue"));
                             resultSet.addElement(LookupElementBuilder.create("break"));
                             resultSet.addElement(LookupElementBuilder.create("end"));
-                            resultSet.addElement(LookupElementBuilder.create("memget"));
-                            resultSet.addElement(LookupElementBuilder.create("memset"));
-                            resultSet.addElement(LookupElementBuilder.create("malloc"));
-                            resultSet.addElement(LookupElementBuilder.create("free"));
+                            resultSet.addElement(LookupElementBuilder.create("memget").withTailText(" <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("memset").withTailText(" <val> <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("malloc").withTailText(" <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("free").withTailText(" <val>"));
                             resultSet.addElement(LookupElementBuilder.create("memdump"));
-                            resultSet.addElement(LookupElementBuilder.create("syscall"));
-                            resultSet.addElement(LookupElementBuilder.create("str"));
-                            resultSet.addElement(LookupElementBuilder.create("throw"));
+                            resultSet.addElement(LookupElementBuilder.create("syscall").withTailText(" <val>"));
+                            resultSet.addElement(LookupElementBuilder.create("str").withTailText(" <string>"));
+                            resultSet.addElement(LookupElementBuilder.create("throw").withTailText(" <val>"));
                     }
                 }
         );
